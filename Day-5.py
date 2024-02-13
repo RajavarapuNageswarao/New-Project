43afac98e22575b64bdaec698bb36fff7d3c4ccd
RajavarapuNageswarao <rajavarapumunna@gmail.com>
2024-02-13 12:51:10 +0530

    commit

--- Day-5.py
+++ Day-5.py
@@ -1,13 +1,48 @@
 #average height
-import random
-student_height_test = [random.randint(156, 178) for i in range(5)]
-total_height = 0
-no_of_students = 0
-for height in student_height_test:
-  total_height+=height
-  no_of_students+=1
-print(f"total height {total_height}")
-print(f"no of students {no_of_students}")
-#find average
-average_students = total_height / no_of_students
-print(f"average height {average_students}")
+# print("This is the average height")
+# import random
+# student_height_test = [random.randint(156, 178) for i in range(5)]
+# total_height = 0
+# no_of_students = 0
+# for height in student_height_test:
+#   total_height+=height
+#   no_of_students+=1
+# print(f"total height {total_height}")
+# print(f"no of students {no_of_students}")
+# #find average
+# average_students = total_height / no_of_students
+# print(f"average height {average_students}")
+
+# student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+# score = 0
+# for score_test in student_scores:
+#   if score_test > score:
+#     score = score_test
+# print(f"Highest score in the class {score}")
+
+#adding sum of even and odd numbers
+# target = int(input("please provide the target number===>"))
+# even_num = 0
+# odd_num = 0
+# for sum_of_even_odd in range(1,target+1):
+#     if sum_of_even_odd % 2 == 0:
+#         even_num += sum_of_even_odd
+#     else:
+#         odd_num += sum_of_even_odd
+# print(f"sum of even number {even_num}")
+# print(f"sum of odd number {odd_num}")            
+
+#fizz game
+# target = 100
+# for game in range(1,target+1):
+#   if game % 3 == 0 and game%5 ==0:
+#     print("fizz")
+#   elif game%3 == 0:
+#     print("fizz")
+#   elif game%5 ==0:
+#     print("buzz")
+#   else:
+#     print(game)      
+
+
+
